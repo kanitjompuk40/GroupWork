619b8aac581a7d991e2d33c3f9dd7f75957385a8
Matthew Wear <mattcanary21@yahoo.co.uk>
2021-02-10 16:55:02 +0000

    Added joining function

--- AML/HelperFunctions.py
+++ AML/HelperFunctions.py
@@ -28,10 +28,46 @@ def meltM5(df, days = 1941, items = 30490):
         Sales data in a melted format
     """
     
+    #Random sample of items items
     df_melt = df.sample(n=items).copy().reset_index(drop=True)
+    #Subset to include specified number of days
     df_melt = df_melt.iloc[:, :days+6]
-    
+    #Melt
     df_melt = pd.melt(df_melt, id_vars=['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], 
                  var_name='d', value_name='sold')
     
     return df_melt
+
+def joinDataSets(melt_df, calendar_df, prices_df):
+    """Function joins melted dataframe with calendar and sell price dataframes
+
+    Parameters
+    ----------
+    melt_df : Pandas DataFrame
+        Melted form of original data set
+    calendar_df : Pandas DataFrame
+        Calendar dataframe
+    prices_df : Pandas DataFrame
+        sell prices dataframe
+
+    Returns
+    -------
+    df_joined
+        Combined all available data into one dataframe in melted format
+    """
+    #Join calendar data
+    df_joined = pd.merge(melt_df, calendar_df, on='d', how='left')
+    #Join sell prices data
+    df_joined = pd.merge(df_joined, prices_df, on=['store_id','item_id','wm_yr_wk'], how='left') 
+
+    return df_joined
+
+
+
+
+
+
+
+
+
+
