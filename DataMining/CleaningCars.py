25d94cf894467cabef9b4b46732db4423946b579
Diego <diego.unzuet@gmail.com>
2021-02-26 12:06:48 +0000

    diego cleaning

--- DataMining/CleaningCars.py
+++ DataMining/CleaningCars.py
@@ -16,3 +16,20 @@ def remove_columns(df):
     columns = ["Unnamed: 0", "id", "image_url", "VIN", "region_url", "id", "model", "size"]
     df.drop(columns, axis=1, inplace=True)
     return(df)
+
+
+def price_range(df, lower = 0, higher = 60_000, sampling = False):
+    
+    if sampling == True:
+        df = vehicles.sample(n = 40_000)
+    
+    df = df.dropna(subset = ["price"])
+    
+    df = df.loc[df["price"] < higher]
+    df = df.loc[df["price"] >= lower]
+    
+    return(df)
+
+
+def TF-IDF(df):
+    raise NotImplementedError
