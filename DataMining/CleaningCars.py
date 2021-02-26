29e22b5176c21557bcf142a14d14d4413bf9f6c7
Diego <diego.unzuet@gmail.com>
2021-02-26 13:35:30 +0000

    Diego TFIDF Function

--- DataMining/CleaningCars.py
+++ DataMining/CleaningCars.py
@@ -7,21 +7,26 @@ Created on Fri Feb 19 10:38:46 2021
 """
 
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 ### Data Cleaning Functions
 
 # Getting rid of unecesarry columns
 
 def remove_columns(df):
+    
+    """Removes columns we agreed were useless"""
     columns = ["Unnamed: 0", "id", "image_url", "VIN", "region_url", "id", "model", "size"]
     df.drop(columns, axis=1, inplace=True)
     return(df)
 
 
 def price_range(df, lower = 0, higher = 60_000, sampling = False):
-    
+    """
+    Set the lower and upper limits of the price, if sampling true it chooses 40,000 samples at random
+    """
     if sampling == True:
-        df = vehicles.sample(n = 40_000)
+        df = df.sample(n = 40_000)
     
     df = df.dropna(subset = ["price"])
     
@@ -31,5 +36,20 @@ def price_range(df, lower = 0, higher = 60_000, sampling = False):
     return(df)
 
 
-def TF-IDF(df):
-    raise NotImplementedError
+def TF_IDF(df, number = 100):
+    
+    """This function adds the TF-IDF values of the most important words to the dataframe,
+    the number can be chosen above"""
+    
+    """TF - term frequency """
+    
+    """Inverse Document Frequency (IDF)
+    IDF is a measure of how important a term is"""
+    
+    vectorizer = TfidfVectorizer(stop_words='english',max_features = number)
+    sentences  = df["description"].values
+    vectorizer.fit(sentences)
+    vector_spaces = vectorizer.transform(sentences)
+    tfidf = vector_spaces.toarray()
+    df = pd.concat([df, pd.DataFrame(tfidf)], axis = 1)
+    return(df)
