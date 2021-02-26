fa9f6652c673842f52a613f736421e31fb93b0a5
Diego <diego.unzuet@gmail.com>
2021-02-26 11:36:06 +0000

    removing columns

--- DataMining/CleaningCars.py
+++ DataMining/CleaningCars.py
@@ -6,5 +6,13 @@ Created on Fri Feb 19 10:38:46 2021
 @author: mattwear
 """
 
+import pandas as pd
+
 ### Data Cleaning Functions
 
+# Getting rid of unecesarry columns
+
+def remove_columns(df):
+    columns = ["Unnamed: 0", "id", "image_url", "VIN", "region_url", "id", "model", "size"]
+    df.drop(columns, axis=1, inplace=True)
+    return(df)
